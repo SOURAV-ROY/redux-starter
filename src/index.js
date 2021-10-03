8eed1794ce1d57f3d0dbd47240be5363fdeea853
SOURAV-ROY <souravroy.info13@gmail.com>
2021-10-03 23:24:15 +0600

    Use Lodash for short

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import {compose, pipe} from 'lodash/fp'
+
+let input = "  JavaScript  ";
+let output = '<h2>' + input.trim() + '</h2>';
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
+console.log(result);
+
+const transformCompose = compose(wrapInDiv, toLowerCase, trim);
+const transformPipe = pipe(trim, toLowerCase, wrapInDiv);
+
+console.log(transformCompose(input));
+console.log(transformPipe(output));
