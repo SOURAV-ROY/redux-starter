d60c15c658eef256ba12c6e5eaec5cad27b425cf
SOURAV-ROY <souravroy.info13@gmail.com>
2021-10-06 13:49:19 +0600

    bugRemoved & store.subscribe(){} Is Awesome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import store from './store';
 
+// state = reducer(state, action)
+// Notify the subscribers that the state has changed
+
+store.subscribe(() => {
+    console.log('Store Changed!!', store.getState());
+})
+
+// const unsubscribe = store.subscribe(() => {
+//     console.log('Store Changed!!', store.getState());
+// })
+
 store.dispatch({
     type: 'bugAdded',
     payload: {
         description: 'BUG 1 Added'
+    }
+})
+
+// unsubscribe();
+store.dispatch({
+    type: 'bugRemoved',
+    payload: {
+        id: 1,
     }
 })
 console.log(store.getState());
